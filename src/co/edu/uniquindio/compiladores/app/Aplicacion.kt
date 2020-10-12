0fed585fd2b14af5e357701aaa6eb80bbfb8af73
Stefanny Román Bohorquez <sromanb@uqvirtual.edu.co>
2020-10-12 15:39:25 -0500

    Paquete app clase aplicacion y ventana CompiladorDSD

---
+++ src/co/edu/uniquindio/compiladores/app/Aplicacion.kt
@@ -0,0 +1,28 @@
+package co.edu.uniquindio.compiladores.app
+
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+class Aplicacion : Application() {
+
+    override fun start(primaryStage: Stage?) {
+        val loader = FXMLLoader(Aplicacion::class.java.getResource("/Inicio.fxml"))
+        val parent: Parent = loader.load()
+
+        val scene = Scene(parent)
+
+        primaryStage?.scene = scene
+        primaryStage?.title = "Compilador DSD"
+        primaryStage?.show()
+    }
+    companion object{
+        @JvmStatic
+        fun main( args: Array<String>){
+            launch(Aplicacion::class.java)
+        }
+    }
+
+}
