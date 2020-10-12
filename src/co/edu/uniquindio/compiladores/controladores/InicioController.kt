17afacfa90a8837b240bd01b7cce6cfca4e1384b
Stefanny Román Bohorquez <sromanb@uqvirtual.edu.co>
2020-10-12 16:05:25 -0500

    ventana compilador DSD principal

--- src/co/edu/uniquindio/compiladores/controladores/InicioController.kt
+++ src/co/edu/uniquindio/compiladores/controladores/InicioController.kt
@@ -1,5 +1,18 @@
 package co.edu.uniquindio.compiladores.controladores
 
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.control.TextField
+
+
 class InicioController{
 
+    @FXML lateinit var cajon1: TextField
+    @FXML lateinit var cajon2: TextField
+    @FXML
+    fun cambiarValores ( e: ActionEvent){
+        val aux= cajon1.text
+        cajon1.text = cajon2.text
+        cajon2.text = aux
+    }
 }
