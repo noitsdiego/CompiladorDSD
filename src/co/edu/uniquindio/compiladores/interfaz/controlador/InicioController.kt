7ec9190faa864cf6cf9370d1b7104cffc89acebc
Daniel-lp <danielalejandroloaizaperez@gmail.com>
2020-10-14 04:39:22 -0500

    13/10/2020 - 11:37pm
    -Se modifico  el controlador, agregando algunos import necesarios.
    -Se creo un nuevo metodo "inicializable".
    -El controlador extiende de  inicializable
    -Ya se muestra todo en la tabla de la interfaz.

--- src/co/edu/uniquindio/compiladores/interfaz/controlador/InicioController.kt
+++ src/co/edu/uniquindio/compiladores/interfaz/controlador/InicioController.kt
@@ -1,15 +1,35 @@
 package co.edu.uniquindio.compiladores.interfaz.controlador
 
 import co.edu.uniquindio.compiladores.logica.lexico.AnalizadorLexico
+import co.edu.uniquindio.compiladores.logica.lexico.Token
+import javafx.collections.FXCollections
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.control.TableColumn
+import javafx.scene.control.TableView
 import javafx.scene.control.TextArea
+import javafx.scene.control.cell.PropertyValueFactory
+import java.net.URL
+import java.util.*
 
 
-
-class InicioController{
+class InicioController: Initializable {
 
     @FXML lateinit var codigoFuente: TextArea
+    @FXML lateinit var tablaTokens: TableView<Token>
+    @FXML lateinit var colLexema: TableColumn<Token, String>
+    @FXML lateinit var colCategoria: TableColumn<Token, String>
+    @FXML lateinit var colFila: TableColumn<Token, String>
+    @FXML lateinit var colColumna: TableColumn<Token, String>
+
+
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+        colLexema.cellValueFactory = PropertyValueFactory("lexema")
+        colCategoria.cellValueFactory = PropertyValueFactory("categoria")
+        colFila.cellValueFactory = PropertyValueFactory("fila")
+        colColumna.cellValueFactory = PropertyValueFactory("columna")
+    }
 
     fun AnalizarCodigo ( e: ActionEvent){
 
@@ -17,7 +37,7 @@ class InicioController{
 
             val lexico = AnalizadorLexico(codigoFuente.text)
             lexico.analizar()
-
+            tablaTokens.items = FXCollections.observableArrayList(lexico.listaTokens)
             print(lexico.listaTokens)
         }
 
